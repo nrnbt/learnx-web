2e2c5ea4eda6e7beb30fdac948796d646c8a6e6c
naranbat.bya <naranbat.bya@corp.unitel.mn>
2024-08-18 12:34:39 +0800

    rdy for build

--- src/app/api/account/route.ts
+++ src/app/api/account/route.ts
@@ -6,31 +6,6 @@ export async function GET (request: Request): Promise<Response> {
   try {
     const cookies = request.headers.get('Cookie')
 
-    const getCookieValue = (cookieString: string, cookieName: string): string => {
-      const name = cookieName + '='
-      const decodedCookie = decodeURIComponent(cookieString)
-      const cookies = decodedCookie.split(';')
-      for (let i = 0; i < cookies.length; i++) {
-        const cookie = cookies[i].trim()
-        if (cookie.indexOf(name) === 0) {
-          return cookie.substring(name.length, cookie.length)
-        }
-      }
-      return ''
-    }
-
-    // Get the edx-user-info cookie value
-    const edxUserInfoEncoded = getCookieValue(cookies ?? '', 'edx-user-info')
-
-    // Decode the URL-encoded value
-    const edxUserInfoDecoded = decodeURIComponent(edxUserInfoEncoded)
-
-    // Parse the JSON string to an object
-    const edxUserInfoJSON = JSON.parse(edxUserInfoDecoded)
-
-    // Access the learner_profile URL
-    const learnerProfileUrl = edxUserInfoJSON.header_urls.learner_profile
-
     const res = await apiClient.get<CourseInitRes>('/user/v1/accounts/', {
       headers: {
         Cookie: cookies
